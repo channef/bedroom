ba412db0778e310ead91bb3159cf1a7f81c6927b
nfchan <nfchan@sina.com>
2021-03-21 19:39:14 +0800

    '添加了移动端提示信息'

--- src/main.js
+++ src/main.js
@@ -1,3 +1,23 @@
+    //    检测设备类型
+    var ua = window.navigator.userAgent,
+            isQQBrowser = /(?:MQQBrowser | QQ)/.test(ua),
+            isWindowsPhone = /(?:Windows Phone)/.test(ua),
+            isSymbian = /(?:SymbianOS)/.test(ua) || isWindowsPhone,
+            isAndroid = /(?:Android)/.test(ua),
+            isFireFox = /(?:Firefox)/.test(ua),
+            isChrome = /(?:Chrome|CriOS)/.test(ua),
+            isIpad = /(?:iPad|PlayBook)/.test(ua),
+            isTablet = /(?:iPad|PlayBook)/.test(ua)||(isFireFox && /(?:Tablet)/.test(ua)),
+            isSafari = /(?:Safari)/.test(ua),
+            isPhone = /(?:iPhone)/.test(ua) && !isTablet,
+            isOpen= /(?:Opera Mini)/.test(ua),
+            isUC = /(?:UCWEB|UCBrowser)/.test(ua),
+            isPc = !isPhone && !isAndroid && !isSymbian;
+console.log(isPc)
+if (!isPc) {
+    document.body.innerHTML = '<h1>请使用浏览器查看哦</h1>';
+}
+else {
 let chair = document.querySelectorAll('.chair')[0]
 chair.addEventListener('mousemove',function (e) {
     // console.log(chair.className)
@@ -9,3 +29,4 @@ chair.addEventListener('mousemove',function (e) {
         }, 2000);
     }
 })
+}
